cec00a491a71fae9b6d37b0545802489a06f22ff
Aggrron <gsm10forhl2@yandex.ru>
2019-05-29 20:01:40 +0300

    aga

---
+++ www/script.js
@@ -0,0 +1,24 @@
+
+function barcodescanner(){
+	cordova.plugins.barcodeScanner.scan(
+	function (result) {
+		alert("We got a barcode\n" +
+			"Result: " + result.text + "\n" +
+			"Format: " + result.format + "\n" +
+			"Cancelled: " + result.cancelled);
+	},
+	function (error) {
+		alert("Scanning failed: " + error);
+	},
+	{
+	showFlipCameraButton : true,
+	showTorchButton : false,
+	torchOn: false,
+	saveHistory: true,
+	prompt : "Place a barcode inside the scan area",
+	resultDisplayDuration: 500,
+	formats : "QR_CODE,PDF_417",
+	orientation : "portrait",
+	disableSuccessBeep: false }
+);
+}
